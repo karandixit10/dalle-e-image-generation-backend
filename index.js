594ee291ad04cd9b968a97d35db356507ced3e32
Karan Dixit <karandixit10@gmail.com>
2024-06-05 05:17:16 +0530

    Create backend api for dalle to generate images

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import connectDb from './mongodb/connect.js';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+import connectDb from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit : '50mb'}));
+app.use(express.json({ limit: "50mb" }));
 
-app.get('/', async (req, res) => {
-    res.send('Hello from DALL-E!');
-})
+app.use("/api/v1/posts", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
+
+app.get("/", async (req, res) => {
+  res.send("Hello from DALL-E!");
+});
 
 const startServer = async (req, res) => {
-    try{
-        connectDb(process.env.MONGODB_URL);
-        app.listen(8080, () => 
-            console.log('Server is running on port http://localhost:8080'))
-    } catch(e){
-        console.log(e);
-        res.status(500).send('Something went wrong');
-    }
-
-}
+  try {
+    connectDb(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log("Server is running on port http://localhost:8080")
+    );
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Something went wrong");
+  }
+};
 
 startServer();
