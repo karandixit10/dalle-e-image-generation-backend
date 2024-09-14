733ac9a9c83bf7436d1157beaa9662ee0ca2c513
Karan Dixit <karandixit10@gmail.com>
2024-09-14 17:19:10 +0530

    Fixes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -12,6 +12,7 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true
 });
 
 router.route("/").get(async (req, res) => {
